7dd740faaaee4fda9bfef1e40f149a69b2952ed8
Mangalam Kumar <mangalamkumar2002@gmail.com>
2024-05-21 21:29:36 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <div className="bg-slate-200 h-screen w-full">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <div className="bg-white border shadow p-2">
+            <p className="text-2xl font-semibold">Assignment</p>
+        </div>
+    );
+}
